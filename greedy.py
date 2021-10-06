1b4c4b09f6738757634972d01bbe8529b6fe3372
haremeat <dmswjd9341@naver.com>
2021-10-06 14:21:26 +0900

    greedy

--- greedy.py
+++ greedy.py
@@ -1,12 +1,37 @@
 
 # 거스름 돈
-n = 1260
+# n = 1260
+# count = 0
+#
+# array = [500, 100, 50, 10]
+#
+# for coin in array:
+#     count += n  # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
+#     n %= coin
+#
+# print(count)
+
+
+# 첫째 줄에 N과 K가 공백을 기준으로 하여 각각 자연수로 주어집니다.
+# 첫째 줄에 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 횟수의 최솟값을 출력합니다.
+n, k = map(int, input().split())
 count = 0
 
-array = [500, 100, 50, 10]
+# // 버림 나눗셈. 실수가 나오며 소수점 이하는 버린다.
 
-for coin in array:
-    count += n  # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
-    n %= coin
+while True:
+    target = (n // k) * k
+    count += (n - target)
+    n = target
 
+    if n < k:
+        break
+
+    count += 1
+    n //= k
+
+count += (n - 1)
 print(count)
+
+
+
