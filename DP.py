a4ce35b3d956b6bc9a8a640ccd33ee53635869ae
haremeat <dmswjd9341@naver.com>
2021-10-20 17:52:53 +0900

    dp2

--- DP.py
+++ DP.py
@@ -1,14 +1,30 @@
 # 백준 1463
 
-x = int(input())
+# x = int(input())
+#
+# dp = [0, 0, 1, 1]
+#
+# for i in range(4, x + 1):
+#     dp.append(dp[i - 1] + 1)
+#     if i % 3 == 0:
+#         dp[i] = min(dp[i], dp[i // 3] + 1)
+#     if i % 2 == 0:
+#         dp[i] = min(dp[i], dp[i // 2] + 1)
+#
+# print(dp[x])
 
-dp = [0, 0, 1, 1]
 
-for i in range(4, x + 1):
-    dp.append(dp[i - 1] + 1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i // 3] + 1)
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i // 2] + 1)
+# 백준 2156
+n = int(input())
+dp = []
+array = []
 
-print(dp[x])
+dp[n] = dp[n - 1] + dp[n - 3] + array[n]
+dp[n] = dp[n - 2] + array[n]
+
+dp[n] = max()
+
+
+
+
+
