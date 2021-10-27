5ab29b2ad6b5cf92736fe68ef789c89e993f6c33
haremeat <dmswjd9341@naver.com>
2021-10-27 14:10:58 +0900

    currency1

--- DP.py
+++ DP.py
@@ -103,5 +103,19 @@ print(dp[n - 1])
 
 # 효율적인 화폐구성
 
+n, m = map(int, input().split())
 
-# 
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+d = [10001] * (m + 1)
+
+d[0] = 0
+for i in range(n):
+    for j in range(array[i], m + 1):
+        if d[j - array[i]] != 10001:
+            d[j] = min(d[j], d[j - array[i]] + 1)
+
+
+
