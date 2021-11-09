292520db82d9bca34ddcdc16ae5597fb72a7af9a
haremeat <dmswjd9341@naver.com>
2021-11-09 09:34:17 +0900

    correct

--- 2231.py
+++ 2231.py
@@ -1,17 +1,16 @@
 # 분해합
 
-n = 216
+# n = 216
 
-# n = int(input())
-
+n = int(input())
 result = 0
+temp = 0
 
-
-for i in range(1, 1000001):
-    for j in str(i):
-        i += int(j)
-        if i == n:
-            result = j
-
-
-print(result)
+for i in range(1, n + 1):
+    temp = sum(map(int, str(i)))
+    result = i + temp
+    if result == n:
+        print(i)
+        break
+else:
+    print(0)
