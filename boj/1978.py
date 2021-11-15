6ee35aa24f265da4819bfaf4f477d523624e6429
haremeat <dmswjd9341@naver.com>
2021-11-15 10:05:03 +0900

    [boj] 1978 solved

--- boj/1978.py
+++ boj/1978.py
@@ -1,32 +1,33 @@
 # 소수 찾기
 
 # 기본적인 소수 판별 함수
-# def is_prime_number(x):
-#     for i in range(2, x):
-#         if x % i == 0:
-#             return False
-#     return True
+def is_prime_number(x):
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
 
 
 # 틀림.. why?
 
 
+def check(num):
+    if num == 1:
+        return False
+    elif num == 2:
+        return True
+    for i in range(2, num + 1):
+        if num % i == 0:
+            return False
+    return True
 
-# def check(num):
-#     if num == 1:
-#         return False
-#     elif num == 2:
-#         return True
-#     for i in range(2, num + 1):
-#         if num % i == 0:
-#             return False
-#     return True
 
 # 에라토스테네스의 체
 import math
 
 n = int(input())
 sosu = list(map(int, input().split()))
+
 
 def is_prime_number(num, arr):
     flag = 0
@@ -45,4 +46,26 @@ def is_prime_number(num, arr):
 
 print(is_prime_number(n, sosu))
 
-# 틀림..
+# 최종 제출
+
+import sys
+import math
+
+input = sys.stdin.readline
+n = int(input())
+
+num_list = []
+
+num_list = map(int, input().split())
+
+prime_cnt = 0
+
+for i in num_list:
+    flag = 0
+    if i > 1:
+        for j in range(2, int(math.sqrt(i)) + 1):
+            if (i % j == 0):
+                flag += 1
+        if flag == 0:
+            prime_cnt += 1
+print(prime_cnt)
