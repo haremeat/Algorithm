dce77b15b57ee6307f54719c37e2d15538b2acb6
haremeat <dmswjd9341@naver.com>
2021-11-23 11:48:07 +0900

    [boj] 17413 solved

--- boj/17413.py
+++ boj/17413.py
@@ -2,8 +2,8 @@
 import sys
 from collections import deque
 
-# s = sys.stdin.readline().rstrip()
-s = "<ab cd>ef gh<ij kl>"
+s = sys.stdin.readline().rstrip()
+# s = "baekjoon online judge"
 
 deq = deque()
 stack = []
@@ -15,21 +15,30 @@ for i in s:
     if i == '<':
         tag = True
         deq.append(i)
-        while len(deq) == 0:
-            result += deq.popleft()
+        while stack:
+            result += stack.pop()
     elif i == '>':
         tag = False
         deq.append(i)
-        while len(deq) == 0:
+        while deq:
             result += deq.popleft()
+    elif i == ' ':
+        if tag is True:
+            deq.append(i)
+            while deq:
+                result += deq.popleft()
+        else:
+            while stack:
+                result += stack.pop()
+            result += ' '
     else:
         if tag is True:
             deq.append(i)
-            while len(deq) == 0:
-                result += deq.popleft()
         elif tag is False and i != '>':
             stack.append(i)
 
 
+while stack:
+    result += stack.pop()
 
-print(deq)
+print(result)
