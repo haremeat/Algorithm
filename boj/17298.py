b3e40433577a3d1a80d0281fcdf1abd32bd97585
haremeat <dmswjd9341@naver.com>
2021-11-24 10:56:23 +0900

    [boj] 17298 wrong

--- boj/17298.py
+++ boj/17298.py
@@ -5,26 +5,24 @@
 import sys
 
 n = int(sys.stdin.readline())
-arr = list(map(int, input().split()))
+arr = list(map(int, sys.stdin.readline().split()))
 stack = []
-result = []
+result = [-1] * n
 
 for i in range(n):
     stack.append(i)
-    if i < n - 1:
-        if arr[i] < arr[i + 1]:
-            stack.pop()
-            result.insert(i, arr[i + 1])
+    if i < n - 1 and arr[i] < arr[i + 1]:
+        stack.pop()
+        result[i] = arr[i + 1]
 
-            if stack and arr[stack[-1]] < arr[i + 1]:
-                result.insert(stack[-1], arr[i + 1])
-                stack.pop()
+        if stack and arr[stack[-1]] < arr[i + 1]:
+            result[stack[-1]] = arr[i + 1]
+            stack.pop()
 
 # 스택 값이 남아있다면
 if stack:
     for i in stack:
-        result.insert(i, -1)
-
+        result[i] = -1
 
 for i in result:
     print(i, end=" ")
