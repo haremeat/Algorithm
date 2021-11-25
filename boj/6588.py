9209d716773d795502c9e72359b362fbfc4da4b0
haremeat <dmswjd9341@naver.com>
2021-11-25 12:54:44 +0900

    [boj] time error

--- boj/6588.py
+++ boj/6588.py
@@ -3,28 +3,25 @@
 import sys
 import math
 
-# num = 1000000
-num = 100
+num = 1000001
 prime_list = [True] * num
 
-for i in range(1, num + 1):
+for i in range(1, num):
     check = False
     if i > 1:
         for j in range(2, int(math.sqrt(i)) + 1):
             if i % j == 0:
                 check = True
-                prime_list[i - 1] = check
+                prime_list[i] = check
         if check is False:
-            prime_list[i - 1] = check
+            prime_list[i] = check
 
 while True:
     n = int(sys.stdin.readline())
 
     if n == 0:
         break
-    for i in range(2, int(math.sqrt(n)) + 1):
+    for i in range(2, (n // 2) + 1):
         if prime_list[n - i] is False:
-            print("{} = {} + {}".format(n, i + 1, (n - i) - 1))
+            print("{} = {} + {}".format(n, i, n - i))
             break
-        else:
-            print("Goldbach's conjecture is wrong.")
