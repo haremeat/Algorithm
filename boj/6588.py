f9ffcc8618fc325951d85a207f50c964b0c96ca9
haremeat <dmswjd9341@naver.com>
2021-11-25 11:27:49 +0900

    [boj] 6588 mod

--- boj/6588.py
+++ boj/6588.py
@@ -3,17 +3,30 @@
 import sys
 import math
 
-n = int(sys.stdin.readline())
-num_list = map(int, sys.stdin.readline().split())
+# num = 1000001
+num = 1000000
+prime_list = [True] * num
 
-prime_cnt = 0
-
-for i in num_list:
+for i in range(1, num + 1):
     check = False
     if i > 1:
         for j in range(2, int(math.sqrt(i)) + 1):
             if (i % j == 0):
                 check = True
+                prime_list[i - 1] = check
         if check is False:
-            prime_cnt += 1
-print(prime_cnt)
+            prime_list[i - 1] = check
+
+# print(prime_list)
+
+while True:
+    n = int(sys.stdin.readline())
+
+    if n == 0:
+        break
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if prime_list[n - i] is False:
+            print("{} = {} + {}".format(n, i, n - i))
+            break
+        else:
+            print("Goldbach's conjecture is wrong.")
