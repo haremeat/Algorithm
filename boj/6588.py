55601db07e6069966767053f02cccfa09780eadd
haremeat <dmswjd9341@naver.com>
2021-11-25 11:41:03 +0900

    [boj] 6588 예제3 통과못함

--- boj/6588.py
+++ boj/6588.py
@@ -3,21 +3,19 @@
 import sys
 import math
 
-# num = 1000001
-num = 1000000
+# num = 1000000
+num = 100
 prime_list = [True] * num
 
 for i in range(1, num + 1):
     check = False
     if i > 1:
         for j in range(2, int(math.sqrt(i)) + 1):
-            if (i % j == 0):
+            if i % j == 0:
                 check = True
                 prime_list[i - 1] = check
         if check is False:
             prime_list[i - 1] = check
-
-# print(prime_list)
 
 while True:
     n = int(sys.stdin.readline())
@@ -26,7 +24,7 @@ while True:
         break
     for i in range(2, int(math.sqrt(n)) + 1):
         if prime_list[n - i] is False:
-            print("{} = {} + {}".format(n, i, n - i))
+            print("{} = {} + {}".format(n, i + 1, (n - i) - 1))
             break
         else:
             print("Goldbach's conjecture is wrong.")
