3b28fb07f68336ce26012616210ce08189d3f3cb
haremeat <dmswjd9341@naver.com>
2021-11-26 10:46:03 +0900

    [boj] 6588 solved

--- boj/6588.py
+++ boj/6588.py
@@ -1,28 +1,24 @@
 # 골드바흐의 추측
-# 아무리 바꿔봐도 시간초과를 피할 수가 없네
 
 import sys
 import math
 
-num = 1000001
+num = 1000000
 prime_list = [True] * num
 
-for i in range(1, num):
-    check = False
-    if i > 1:
-        for j in range(2, int(math.sqrt(i)) + 1):
-            if i % j == 0:
-                check = True
-                prime_list[i] = check
-        if check is False:
-            prime_list[i] = check
+for i in range(2, int(math.sqrt(num)) + 1):
+    if prime_list[i] is True:
+        for j in range(i * 2, num, i):
+            if prime_list[j] is True:
+                prime_list[j] = False
 
 while True:
     n = int(sys.stdin.readline())
 
     if n == 0:
         break
-    for i in range(2, (n // 2) + 1):
-        if prime_list[n - i] is False:
-            print("{} = {} + {}".format(n, i, n - i))
-            break
+    for i in range(3, num):
+        if prime_list[i] is True:
+            if prime_list[n - i] is True:
+                print("{} = {} + {}".format(n, i, n - i))
+                break
