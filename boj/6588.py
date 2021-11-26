27a0613e975769306a4ff9c7df0de6e4ae6df1ac
haremeat <dmswjd9341@naver.com>
2021-11-26 11:08:37 +0900

    [Read.me] boj 6588

--- boj/6588.py
+++ boj/6588.py
@@ -7,10 +7,9 @@ num = 1000000
 prime_list = [True] * num
 
 for i in range(2, int(math.sqrt(num)) + 1):
-    if prime_list[i] is True:
-        for j in range(i * 2, num, i):
-            if prime_list[j] is True:
-                prime_list[j] = False
+    for j in range(i * 2, num, i):
+        if prime_list[j] is True:
+            prime_list[j] = False
 
 while True:
     n = int(sys.stdin.readline())
@@ -18,7 +17,6 @@ while True:
     if n == 0:
         break
     for i in range(3, num):
-        if prime_list[i] is True:
-            if prime_list[n - i] is True:
-                print("{} = {} + {}".format(n, i, n - i))
-                break
+        if prime_list[i] is True and prime_list[n - i] is True:
+            print("{} = {} + {}".format(n, i, n - i))
+            break
--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 # 연습용 노트
 
-# people = [int(input()) for _ in range(9)]
+a = int(input())
+b = 0
 
-people = [20, 7, 23, 19, 10, 15, 25, 8, 13]
+for i in range(2, a):
+    if a % i == 0:
+        b = 1
 
-total = sum(people)
-fake0 = 0
-fake1 = 0
 
-for i in range(8):
-    for j in (i+1, 9):
-        if total - (people[i] + people[j]) == 100:
-            fake0 = people[i]
-            fake1 = people[j]
+if b == 1 or a == 1 or a == 0:
+    print("소수가 아님")
+else:
+    print("소수임")
 
-people.remove(fake0)
-people.remove(fake1)
-people.sort()
 
-for i in people:
-    print(i)
