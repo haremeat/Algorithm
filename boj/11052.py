44b51d7f57d6a2c1d6429b9211db024d759eb419
haremeat <dmswjd9341@naver.com>
2021-12-03 10:19:49 +0900

    [boj] 11052 not solved

--- boj/11052.py
+++ boj/11052.py
@@ -1,2 +1,17 @@
 # 카드 구매하기
 
+import sys
+n = int(sys.stdin.readline())
+cards = list(map(int, sys.stdin.readline().split()))
+dp = [0] * (n + 1)
+
+
+dp[1] = cards[0]
+
+for i in range(2, n + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + cards[i])
+
+
+print(dp[n])
+
