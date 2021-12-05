64cf333c9954f48aeee90bc88389fd4135824a8a
haremeat <dmswjd9341@naver.com>
2021-12-05 18:35:49 +0900

    [boj] 11052 mod

--- boj/11052.py
+++ boj/11052.py
@@ -7,8 +7,9 @@ dp = [0] * (n + 1)
 
 
 dp[1] = cards[0]
+dp[2] = max(cards[0] + cards[1], cards[0])
 
-for i in range(2, n + 1):
+for i in range(3, n + 1):
     for j in range(1, i + 1):
         dp[i] = max(dp[i], dp[i - j] + cards[i])
 
