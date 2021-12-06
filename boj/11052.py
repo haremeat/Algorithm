02e355fe2e02f0f047e270182eb048b05da8592e
haremeat <dmswjd9341@naver.com>
2021-12-06 14:52:19 +0900

    [boj] 11052 solved

--- boj/11052.py
+++ boj/11052.py
@@ -2,17 +2,15 @@
 
 import sys
 n = int(sys.stdin.readline())
-cards = list(map(int, sys.stdin.readline().split()))
+cards = [0] + list(map(int, sys.stdin.readline().split()))
 dp = [0] * (n + 1)
 
 
-dp[1] = cards[0]
-dp[2] = max(cards[0] + cards[1], cards[0])
+dp[1] = cards[1]
 
-for i in range(3, n + 1):
+for i in range(2, n + 1):
     for j in range(1, i + 1):
-        dp[i] = max(dp[i], dp[i - j] + cards[i])
-
+        dp[i] = max(dp[i], dp[i-j] + cards[j])
 
 print(dp[n])
 
