d37040fab851e15699d1f579adbadaf899d55a81
haremeat <dmswjd9341@naver.com>
2021-12-17 10:57:57 +0900

    [boj] 1260 mod

--- boj/1260.py
+++ boj/1260.py
@@ -4,51 +4,6 @@
 # 간선의 개수 m
 # 탐색을 시작할 정점의 번호 v
 
-
-# test1
-
-
-# from collections import deque
-#
-# n, m, v = map(int, input().split())
-#
-# graph = [[0] * (n + 1) for _ in range(n + 1)]
-#
-# for i in range(m):
-#     m1, m2 = int(input().split())
-#     graph[m1][m2] = graph[m2][m1] = 1
-#
-#
-# def bfs(start, visited):
-#     queue = deque([start])
-#
-#     visited[start] = True
-#
-#     while queue:
-#         v = queue.popleft()
-#         print(v, end=' ')
-#
-#         for i in graph[v]:
-#             if not visited[i]:
-#                 queue.append(i)
-#                 visited[i] = True
-#
-#
-# def dfs(v, visited=[]):
-#     # 현재 노드를 방문처리
-#     visited[v] = True
-#     print(v, end=' ')
-#
-#     # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-#     for i in graph[v]:
-#         if not visited[i]:
-#             dfs(graph, i, visited)
-#
-#
-# dfs(v)
-# print()
-# bfs(v)
-
 from collections import deque
 
 n, m, v = map(int, input().split())
